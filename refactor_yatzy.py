a02ce584496fc2d11afced07ec520bb217dc8270
Ramon <rsanchezabadia@cifpfbmoll.eu>
2022-01-09 03:30:42 +0100

    refactor: :recycle: Yatzy Refactored

--- refactor_yatzy.py
+++ refactor_yatzy.py
@@ -1,3 +1,4 @@
+import collections
 class Yatzy:
     @staticmethod
     def oneList(d1, d2, d3, d4, d5):
@@ -42,164 +43,91 @@ class Yatzy:
                 sum += 3
         return sum
     
-
-    def __init__(self, d1, d2, d3, d4, _5):
-        self.dice = [0]*5
-        self.dice[0] = d1
-        self.dice[1] = d2
-        self.dice[2] = d3
-        self.dice[3] = d4
-        self.dice[4] = _5
-    
-    def fours(self):
+    @staticmethod
+    def fours(diceList):
         sum = 0
-        for at in range(5):
-            if (self.dice[at] == 4): 
+        for dice in diceList:
+            if dice == 4:
                 sum += 4
         return sum
     
-
-    def fives(self):
-        s = 0
-        i = 0
-        for i in range(len(self.dice)): 
-            if (self.dice[i] == 5):
-                s = s + 5
-        return s
-    
-
-    def sixes(self):
+    @staticmethod
+    def fives(diceList):
         sum = 0
-        for at in range(len(self.dice)): 
-            if (self.dice[at] == 6):
-                sum = sum + 6
+        for dice in diceList:
+            if dice == 5:
+                sum += 5
         return sum
     
     @staticmethod
-    def score_pair( d1,  d2,  d3,  d4,  d5):
-        counts = [0]*6
-        counts[d1-1] += 1
-        counts[d2-1] += 1
-        counts[d3-1] += 1
-        counts[d4-1] += 1
-        counts[d5-1] += 1
-        at = 0
-        for at in range(6):
-            if (counts[6-at-1] == 2):
-                return (6-at)*2
-        return 0
+    def sixes(diceList):
+        sum = 0
+        for dice in diceList:
+            if dice == 6:
+                sum += 6
+        return sum
     
     @staticmethod
-    def two_pair( d1,  d2,  d3,  d4,  d5):
-        counts = [0]*6
-        counts[d1-1] += 1
-        counts[d2-1] += 1
-        counts[d3-1] += 1
-        counts[d4-1] += 1
-        counts[d5-1] += 1
-        n = 0
-        score = 0
-        for i in range(6):
-            if (counts[6-i-1] >= 2):
-                n = n+1
-                score += (6-i)
-                    
-        if (n == 2):
-            return score * 2
-        else:
-            return 0
-    
-    @staticmethod
-    def four_of_a_kind( _1,  _2,  d3,  d4,  d5):
-        tallies = [0]*6
-        tallies[_1-1] += 1
-        tallies[_2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-        for i in range(6):
-            if (tallies[i] >= 4):
-                return (i+1) * 4
+    def score_pair(diceList):
+        numbersDice = [6,5,4,3,2,1]
+        for num in numbersDice:
+            if diceList.count(num) >= 2:
+                return num * 2
         return 0
-    
+            
+    @staticmethod
+    def two_pair(diceList):
+        numbersDice = [6,5,4,3,2,1]
+        sum = 0
+        total = 0
+        for num in numbersDice:
+            if diceList.count(num) >= 2:
+                sum += num * 2
+                total += 1
+            elif total == 2:
+                return sum
+        return 0
 
     @staticmethod
-    def three_of_a_kind( d1,  d2,  d3,  d4,  d5):
-        t = [0]*6
-        t[d1-1] += 1
-        t[d2-1] += 1
-        t[d3-1] += 1
-        t[d4-1] += 1
-        t[d5-1] += 1
-        for i in range(6):
-            if (t[i] >= 3):
-                return (i+1) * 3
+    def three_of_a_kind(diceList):
+        x = 0
+        for dice in diceList:
+            if diceList.count(diceList[x]) >= 3:
+                return(dice * 3)
+            x += 1
         return 0
     
+    @staticmethod
+    def four_of_a_kind(diceList):
+        for dice in diceList:
+            if diceList.count(diceList[0]) < 4 and diceList.count(diceList[1]) < 4:
+                return 0
+            else:
+                return(dice * 4)
 
     @staticmethod
-    def smallStraight( d1,  d2,  d3,  d4,  d5):
-        tallies = [0]*6
-        tallies[d1-1] += 1
-        tallies[d2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-        if (tallies[0] == 1 and
-            tallies[1] == 1 and
-            tallies[2] == 1 and
-            tallies[3] == 1 and
-            tallies[4] == 1):
+    def smallStraight(diceList):
+        rightList = [1,2,3,4,5]
+        if collections.Counter(diceList) == collections.Counter(rightList):
             return 15
         return 0
-    
 
     @staticmethod
-    def largeStraight( d1,  d2,  d3,  d4,  d5):
-        tallies = [0]*6
-        tallies[d1-1] += 1
-        tallies[d2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-        if (tallies[1] == 1 and
-            tallies[2] == 1 and
-            tallies[3] == 1 and
-            tallies[4] == 1
-            and tallies[5] == 1):
+    def largeStraight(diceList):
+        rightList = [2,3,4,5,6]
+        if collections.Counter(diceList) == collections.Counter(rightList):
             return 20
         return 0
     
-
     @staticmethod
-    def fullHouse( d1,  d2,  d3,  d4,  d5):
-        tallies = []
-        _2 = False
-        i = 0
-        _2_at = 0
-        _3 = False
-        _3_at = 0
-
-        tallies = [0]*6
-        tallies[d1-1] += 1
-        tallies[d2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-
-        for i in range(6):
-            if (tallies[i] == 2): 
-                _2 = True
-                _2_at = i+1
-            
-
-        for i in range(6):
-            if (tallies[i] == 3): 
-                _3 = True
-                _3_at = i+1
-            
-
-        if (_2 and _3):
-            return _2_at * 2 + _3_at * 3
-        else:
-            return 0
+    def fullHouse(diceList):
+        x = 0
+        for dice in diceList:
+            if diceList.count(diceList[x]) == 3:
+                j = 0
+                for dice1 in diceList:
+                    if diceList.count(diceList[j]) == 2 and dice1 != dice:
+                        return (dice * 3 + dice1 * 2)
+                    j += 1
+            x += 1
+        return 0
